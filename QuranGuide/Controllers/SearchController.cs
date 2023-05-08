75987834f29753efb68c16dd958f7161ba7e2bd9
Amr Mohsen <Amr Mohsen@DESKTOP-0S3I3B8>
2023-05-09 02:04:22 +0300

    Add project files.

---
+++ QuranGuide/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuranGuide.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult search_verse()
+        {
+            return View();
+        }
+    }
+}
